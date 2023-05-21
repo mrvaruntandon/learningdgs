efb096a19192ddae70cfbf4f6e016e36e8de9226
Varun Tandon <mrvaruntandon@gmail.com>
2023-05-21 10:10:09 +0530

    project scaffold created from start.spring.io

---
+++ src/main/java/com/github/mrvaruntandon/learningdgs/LearningdgsApplication.java
@@ -0,0 +1,13 @@
+package com.github.mrvaruntandon.learningdgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningdgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningdgsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mrvaruntandon/learningdgs/LearningdgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mrvaruntandon.learningdgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningdgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
